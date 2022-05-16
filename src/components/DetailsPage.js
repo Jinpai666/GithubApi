c272faa5652342e346e967ef03b0b3c65847c3ec
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-05-16 10:29:02 +0200

    create detail display logic

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -9,25 +9,35 @@ export default function DetailsPage(){
     }
     const {id} = useParams();
     const data = JSON.parse(localStorage.getItem('searchData'));
-    const repository = data && data.filter(repo => Number(repo.id) === Number(id.slice(3)))[0];
+    const repository = data.filter(repo => repo.id === Number(id.slice(3)))[0];
+    const favourites = JSON.parse(localStorage.getItem('fav-repos'));
+    const combined = repository ? [...favourites,repository] : [...favourites];
+    const filtered = combined ? combined.filter(repo => repo.id === Number(id.slice(3))) :  favourites;
+    const final = filtered[0];
     return  (
         <>
-            {repository && repository.length !== 0 && <div className={"details"}>
+            {final && final.length !== 0 && <div className={"details"}>
                 <div className={"details__info"}>
-                    <span className={"details__name"}>{repository.name}</span>
-                    by {repository.owner.login}
+                    <span className={"details__name"}>{final.name}</span>
+                    by {final.owner.login}
                     <img
                     className={"details__avatar"}
-                    src={repository.owner.avatar_url}
+                    src={final.owner.avatar_url}
                     alt="owner avatar"
                     />
                 </div>
-                <p className={"details__description"}>{repository.description}</p>
-                <Link className={"details__link"} to={repository.url}>&#x1F517; URL</Link>
-                <p className={"details__stars"}>&#x2B50; {repository.stargazers_count}</p>
-                <p className={"details__date"}>&#x1F382; {getDate(repository.created_at)}</p>
-                {/*<button onClick={()=>console.log(repository)}>test</button>*/}
+                <p className={"details__description"}>{final.description}</p>
+                <Link className={"details__link"} to={final.url}>&#x1F517; URL</Link>
+                <p className={"details__stars"}>&#x2B50; {final.stargazers_count}</p>
+                <p className={"details__date"}>&#x1F382; {getDate(final.created_at)}</p>
             </div>}
+            <button onClick={()=>console.log(repository)}>data</button>
+            <button onClick={()=>console.log(repository)}>repository</button>
+            <button onClick={()=>console.log(favourites)}>favourites</button>
+            <button onClick={()=>console.log(combined)}>combined</button>
+            <button onClick={()=>console.log(filtered)}>filtered</button>
+            <button onClick={()=>console.log(final)}>final</button>
+
         </>
 
     )
