0827821f7776a611f7055297a30b7127e9bb552c
Jinpai666 <41897625+Jinpai666@users.noreply.github.com>
2022-05-16 11:30:12 +0200

    change size of table rows, start logic for sorting arrow

--- src/components/MainTable.js
+++ src/components/MainTable.js
@@ -3,7 +3,9 @@ import ResultTable from "./ResultTable";
 
 export default function MainTable({database, setDatabase, pagesVisited, rowsPerPage}){
     const [order, setOrder] = useState("ascending");
+
 //sorting functions
+    const triangle = order === 'ascending' ? "&#x1F53A;" : "&#x1F53B;"
     const sortNumbers = (column) => {
         if (order === "ascending") {
             const sorted = [...database].sort((a,b) =>
@@ -52,16 +54,29 @@ export default function MainTable({database, setDatabase, pagesVisited, rowsPerP
             setOrder("ascending");
         }
     }
-
+//handle clicks
+   const [clicked,setClicked] = useState(null)
+    const handleNumbersClick =  (event, sortBy) => {
+        sortNumbers(sortBy);
+        setClicked(event.target.innerHTML)
+    }
+    const handleNameClick =  (event, sortBy) => {
+        sortName(sortBy);
+        setClicked(event.target.innerHTML)
+    }
+    const handleOwnerClick =  (event, sortBy) => {
+        sortOwner(sortBy);
+        setClicked(event.target.innerHTML)
+    }
     return (
         <table>
             <thead className={"main__table"} >
                 <tr className={"main__tableHead"} >
-                    <th className={"main__tableCell"} onClick={database ? () => sortNumbers('id') : null}>ID</th>
-                    <th className={"main__tableCell"} onClick={database ? () => sortName("name") : null}>Nazwa repozytorium</th>
-                    <th className={"main__tableCell"} onClick={database ? () => sortOwner() : null}>Właściciel</th>
-                    <th className={"main__tableCell"} onClick={database ? () => sortNumbers("stargazers_count") : null}>Liczba gwiazdek</th>
-                    <th className={"main__tableCell"} onClick={database ? () => sortNumbers("created_at") : null}>Data utworzenia</th>
+                    <th className={"main__tableCell"} onClick={database ? (event) => handleNumbersClick(event,'id') : null}> ID </th>
+                    <th className={"main__tableCell"} onClick={database ? (event) => handleNameClick(event) : null}>Nazwa repozytorium</th>
+                    <th className={"main__tableCell"} onClick={database ? (event) => handleOwnerClick(event) : null}>Właściciel</th>
+                    <th className={"main__tableCell"} onClick={database ? (event) => handleNumbersClick(event,'stargazers_count') : null}>Liczba gwiazdek </th>
+                    <th className={"main__tableCell"} onClick={database ? (event) => handleNumbersClick(event,'created_at') : null}>Data utworzenia </th>
                     <th className={"main__tableCell"}>Ulubione</th>
                 </tr>
             </thead>
@@ -70,6 +85,7 @@ export default function MainTable({database, setDatabase, pagesVisited, rowsPerP
                 pagesVisited={ pagesVisited }
                 rowsPerPage={ rowsPerPage }
             />
+            <button onClick={() => console.log(clicked)}>test</button>
         </table>
     )
 }
